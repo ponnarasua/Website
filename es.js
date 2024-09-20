c7a19cc4311794ab0ac44acfb389ab29f5f28583
Ponnarasu <ponnarasua22it@srishakthi.ac.in>
2024-09-20 23:44:27 +0530

    Update es.js

--- es.js
+++ es.js
@@ -6,3 +6,10 @@ const f2 = () => {
 }
 f1();
 f2();
+let arr= [2,4,8,10]
+let updatedArr = arr.map(val=> val+2)
+console.log(arr);
+console.log(updatedArr);
+let updatedArr3 = arr.slice().filter(val => val < 5);
+console.log(arr);
+console.log(updatedArr3);
